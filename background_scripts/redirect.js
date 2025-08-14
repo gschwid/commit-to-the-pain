465d3ca6eda45cf5423b048b9438987cc1f3063d
Grant Schwidder <gschwid@gmail.com>
2025-08-13 21:28:29 -0500

    Have any request redirect to leetcode webstie

--- background_scripts/redirect.js
+++ background_scripts/redirect.js
@@ -1,5 +1,12 @@
-browser.webNavigation.onCommitted.addListener(
-  () => {
-    console.log("Going to a new link...");
+function logURL(requestDetails) {
+  if (!requestDetails.url.includes("leetcode")) {
+console.log(`Loading: ${requestDetails.url}`);
+  return { redirectUrl: "https://leetcode.com/",};
   }
-);
+  console.log("nothing to do already on leetcode")
+}
+
+browser.webRequest.onBeforeRequest.addListener(logURL, {
+  urls: ["<all_urls>"],
+},
+['blocking']);
